8330270d32cb10e8e2f033ac004ab69b18c42a25
Jhoni Cunha <jhonicunha@gmail.com>
2025-09-04 16:13:55 -0300

    Nível Aventureiro Concluído

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,6 @@
 // Desafio Movimentação de Peças de Xadrez - Nível Novato
 
 int main() {
-    
     // Movimento da Torre (for)
     printf("Movimento da Torre (5 casas para a direita)\n");
     int i_torre;
@@ -33,5 +32,23 @@ int main() {
         movimentos_bispo++;
     } while (movimentos_bispo < 5);
 
+    // Espaço
+    printf("\n");
+
+    // Movimento do Cavalo (loops aninhados)
+    printf("Movimento do Cavalo (2 casas para baixo, 1 para a esquerda)\n");
+    int i_cavalo;
+    for (i_cavalo = 0; i_cavalo < 2; i_cavalo++) {
+        if (i_cavalo == 0) {
+            int j_cavalo = 0;
+            while (j_cavalo < 2) {
+                printf("Baixo\n");
+                j_cavalo++;
+            }
+        }
+        else {
+            printf("Esquerda\n");
+        }
+    }
     return 0;
 }
